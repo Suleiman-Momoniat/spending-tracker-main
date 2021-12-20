17d704272043bca7dc916c3a3d8f4a2156253315
Suleiman <suleiman.momoniat@gmail.com>
2021-12-20 11:01:41 +0200

    feat(app):dynamically add expense

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,44 +1,32 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
 
 const Expenses = (props) => {
+  const [filterDate, setFilterDate] = useState("2020");
 
-    const [filterDate, setFilterDate] = useState('2020');
+  const filterHandler = (filteredDate) => {
+    setFilterDate(filteredDate);
+  };
 
-    const filterHandler = (filteredDate) => {
-        setFilterDate(filteredDate);
-    }
-
-    return (
-        <div>
-            <ExpensesFilter value={filterDate} onChangeFilter={filterHandler} />
-            <Card className="expenses">
-                <ExpenseItem
-                title={props.expenses[0].title}
-                amount={props.expenses[0].amount}
-                date={props.expenses[0].date}
-                />
-                <ExpenseItem
-                    title={props.expenses[1].title}
-                    amount={props.expenses[1].amount}
-                    date={props.expenses[1].date}
-                />
-                <ExpenseItem
-                    title={props.expenses[2].title}
-                    amount={props.expenses[2].amount}
-                    date={props.expenses[2].date}
-                />
-                <ExpenseItem
-                    title={props.expenses[3].title}
-                    amount={props.expenses[3].amount}
-                    date={props.expenses[3].date}
-                />
-        </Card>
-      </div>
-    );
-}
+  return (
+    <div>
+      <ExpensesFilter value={filterDate} onChangeFilter={filterHandler} />
+      <Card className="expenses">
+        {props.expenses.map((expenseItem) => {
+          return (
+            <ExpenseItem
+              title={expenseItem.title}
+              amount={expenseItem.amount}
+              date={expenseItem.date}
+            />
+          );
+        })}
+      </Card>
+    </div>
+  );
+};
 
 export default Expenses;
